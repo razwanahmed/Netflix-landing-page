fc95f5c754b72cefb0c9dba545b427c2e024327c
Razwan Ahmed <mdriyad0439@gmail.com>
2021-05-27 03:23:04 +0600

    New folder

---
+++ Netflex Style.js
@@ -0,0 +1,25 @@
+
+function togglevideo(){
+    const trailer = document.querySelector (".trailer");
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
